761dcee4c211aaf7fb4958abb44b85d919a37b04
Julian Londoño <jdavid.londono1@udea.edu.co>
2021-04-19 23:15:25 -0500

    funcion en qt

---
+++ funcionImagenParcial/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char a[8];
+    int imagen[8][8];
+    for (int i=0; i<64; i++) *(*(imagen)+i)='*';
+    cout << "El ingreso se hace por renglones, por lo tanto son 8 " << endl;
+    cout << "Ingrese * para leds apagados y o para leds encendidos " << endl;
+    for (int j=0; j<8; j++)
+    {
+        cout << "Ingrese la fila " << j << endl;
+
+        for (int i=0;i<8; i++) cin >> a[i];
+
+        for (int i=0;i<8; i++)
+        {
+            if (a[i]=='*') *(*(imagen)+j+i*8)=0;
+            else if ( a[i]=='o') *(*(imagen)+j+i*8)=1;
+        }
+    }
+    for (int i=0; i<8; i++)
+    {
+        for (int j=0; j<8; j++)
+        {
+            cout <<imagen[j][i];
+        }
+        cout << endl;
+    }
+    cout << endl;
+    return 0;
+
+}
