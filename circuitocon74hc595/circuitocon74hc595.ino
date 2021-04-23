9fb27f8fa8bd928366b337299ef2505a4af64013
Julian Londoño <jdavid.londono1@udea.edu.co>
2021-04-23 14:33:53 -0500

    avance en imagen

--- circuitocon74hc595/circuitocon74hc595.ino
+++ circuitocon74hc595/circuitocon74hc595.ino
@@ -11,6 +11,7 @@ bool seleccion;
 int opcion;
 char m[8];
 int contador_char = 0;
+int j = 0;
 
 //DECLARACION DE FUNCIONES (PROTOTIPOS
 
@@ -116,54 +117,79 @@ void imagen(int &opcion)
 
   //Serial.println("Ingrese la fila");
   char f = 'x';
+
   if (contador_char < 8) {
-    f = Serial.read();
-    if ( f == '*' ||  f == 'o') {
-      //for (contador_char; contador_char < 8; contador_char++) {
-      m[contador_char] = f;
-      Serial.println(m[contador_char]);
-      Serial.println(contador_char);
-      contador_char ++;
-      f = 'x';
+    if (j == 0)
+    {
+      f = Serial.read();
+      if ( f == '*' ||  f == 'o') {
+        //for (contador_char; contador_char < 8; contador_char++) {
+        m[contador_char] = f;
+        //Serial.println(m[contador_char]);
+        //Serial.println(contador_char);
+        contador_char ++;
+        f = 'x';
+      }
+      if (contador_char == 7)
+      {
+        for (int i = 0; i < 8; i++)
+        {
+          if (m[i] == '*')
+            *(*(a) + i) = 0;
+
+
+          else
+            *(*(a) + i) = 1;
+          if (i == 7) {
+            for (int i = 0; i < 64; i++)
+            {
+              Serial.print(*(*(a) + i));
+              Serial.print(' ');
+              if ((i + 1) % 8 == 0) Serial.println(' ');
+              j++;
+            }
+          }
+        }
+      }
+
+    }
+    else {
+      opcion = 0;
+      opcion_menu = true;
+      contador_char = 0;
     }
 
-  }
-  else {
-    opcion = 0;
-    opcion_menu = true;
-    contador_char = 0;
-  }
-
-  /* //if (Serial.available() == 0)
-    {
-     for (contador_char; contador_char < 8; contador_char++) {
-       m[contador_char] = Serial.read();
-       Serial.println(m[contador_char]);
-       Serial.println(contador_char);
-     }
-
-     if (contador_char == 7)
-     {
-       opcion = 0;
-       opcion_menu = true;
-     }
-     /*  //for (int i = 0; i < 64; i++) {
-
-       digitalWrite(SER, *(*(a) + i));
-
-       clock();
-
+    /* //if (Serial.available() == 0)
+      {
+       for (contador_char; contador_char < 8; contador_char++) {
+         m[contador_char] = Serial.read();
+         Serial.println(m[contador_char]);
+         Serial.println(contador_char);
        }
-       digitalWrite(SRCLK, HIGH);
-       delay(2000);
-       digitalWrite(SRCLK, LOW);
-       for (int i = 0; i < 64; i++)*(*(a) + i) = 0;
-       for (int i = 0; i < 64; i++) {
 
-       digitalWrite(SER, *(*(a) + i));
-       clock();
+       if (contador_char == 7)
+       {
+         opcion = 0;
+         opcion_menu = true;
        }
-       digitalWrite(SRCLK, HIGH);
-       //  opcion = 0;
-  */
+       /*  //for (int i = 0; i < 64; i++) {
+
+         digitalWrite(SER, *(*(a) + i));
+
+         clock();
+
+         }
+         digitalWrite(SRCLK, HIGH);
+         delay(2000);
+         digitalWrite(SRCLK, LOW);
+         for (int i = 0; i < 64; i++)*(*(a) + i) = 0;
+         for (int i = 0; i < 64; i++) {
+
+         digitalWrite(SER, *(*(a) + i));
+         clock();
+         }
+         digitalWrite(SRCLK, HIGH);
+         //  opcion = 0;
+    */
+  }
 }
